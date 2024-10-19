56481204eb61430bf462cfa2960bef9e895a0525
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-10-19 18:44:56 +0700

    refactor: improve with clean code

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() => runApp(const TravelApp());
 
 class TravelApp extends StatelessWidget {
   const TravelApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
